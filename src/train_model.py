2666ced0e8bb1454468d254279ff18ff50afcecc
unknown <d.querales@gmail.com>
2023-10-15 12:09:08 -0400

    fix: add model as json

--- src/train_model.py
+++ src/train_model.py
@@ -21,8 +21,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle
 
 # train model
 model = xgb.XGBRegressor(n_estimators=1000)
-model.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=50, verbose=False)
-model.save_model("models/model.json")
+model.fit(X_train, y_train)
+model.save_model("../models/model.json")
 
 # predict target
 y_pred = model.predict(X_test) 
